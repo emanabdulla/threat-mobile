5f88a37105e6cecacd0082fc6ffd7d46c14d537e
emanabdulla <emme-666@hotmail.com>
2022-02-08 16:17:46 +0000

    Updated main

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "An ontology for mobilethreat"
   :dependencies [[uk.org.russet/tawny-owl "2.0.0-SNAPSHOT"]
                  [org.clojure/clojure "1.10.1"]]
-  :main mobilethreat.core
+  :main mobilethreat.mobilethreat
 
   :profiles
   {:light {:plugins [[nightlight/lein-nightlight "1.9.0"]]}}
--- src/mobilethreat/mobilethreat.clj
+++ src/mobilethreat/mobilethreat.clj
@@ -5,14 +5,50 @@
             [tawny.reasoner :as r]))
 
 
-(defontology mobilethreat
-  :iri "http://www.russet.org.uk/tawny/mobilethreat/mobilethreat"
+(defontology mThreat
+  :iri "http://www.russet.org.uk/tawny/mobilethreat/mthreat"
   :comment "An ontology for mobile threat Catalogue (MTC), which describes, identifies, and structures the threats posed to mobile information systems."
-)
+  )
 
 (r/reasoner-factory :hermit)
 
+;;OWL CLASSES
+
 (defclass threat
-:comment "The threat is a potential negative action or event facilitated by a vulnerability that results in an unwanted impact on a computer system, application and mobile devices.")
+  :comment "The threat is a potential negative action or event facilitated by a vulnerability that results in an unwanted impact on a computer system, application and mobile devices.")
 
 (defclass threatCategories)
+(defoproperty hasCategory
+  :domain threat
+  :range threatCategories)
+
+(as-disjoint-subclasses
+ threatCategories
+ (defclass Application
+   :comment "Threats related to software applications developed for mobile devices")
+ (defclass Authentication
+   :comment "Threats related to authentication mechanisms.")
+ (defclass Cellular
+   :comment "Threats related to cellular systems and infrastructure.")
+ (defclass Ecosystem
+   :comment "Threats related to the greater mobile ecosystem.")
+ (defclass EMM
+   :comment "Threats related to enterprise mobility management systems.")
+ (defclass GPS
+   :comment "Threats related to GPS technology.")
+ (defclass LAN&PAN
+   :comment "Threats related to local and personal area networks.")
+ (defclass Payment
+   :comment "Threats related to mobile payments.")
+ (defclass PhysicalAccess
+   :comment "Threats originating from outside of the device")
+ (defclass Privacy
+   :comment "Threats related to user privacy")
+ (defclass Stack
+   :comment "Threats related to the hardware, firmware, and software used to host and operate a device")
+ (defclass SupplyChain
+   :comment "Threats related to the device and component supply chain."))
+
+
+(save-ontology "mobilethreat.omn" :omn)
+(save-ontology "mobilethreat.owl" :owl)
